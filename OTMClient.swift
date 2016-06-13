92564ad3a2b63169acc4bb78c401931bac9cfd4a
Eman <emmanuoel.h@gmail.com>
2016-06-13 02:40:09 -0700

    Create POST and urlFromComponents functions

--- OTMClient.swift
+++ OTMClient.swift
@@ -10,32 +10,77 @@ import Foundation
 
 class OTMClient: NSObject {
     
-    
-    func taskForGetMethod(method: String, parameters: [String: AnyObject]) {
+    func taskForGETMethod (method: String, parameters: [String: AnyObject], completionHandler: (result: AnyObject?, error: String?) -> Void) -> NSURLSessionDataTask {
         
-        guard let url = buildURL(Constants.Host, method: method, parameters: parameters) else {
-            print("URL did not build")
-            return
+        let url = urlFromComponents("https", host: Constants.Host, method: method, parameters: parameters)
+        let request = NSURLRequest(URL: url)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            (data, response, error) in
+            
+            
         }
+        task.resume()
+        return task
+    }
+    
+    func taskForPOSTMethod(method: String, parameters: [String: AnyObject], jsonBody: String, completionHandlerForPOST: (result: AnyObject?, error: String?)->Void) -> NSURLSessionDataTask {
+        
+        let url = urlFromComponents("https", host: Constants.Host, method: method, parameters: parameters)
         let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = jsonBody.dataUsingEncoding(NSUTF8StringEncoding)
         
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            (data, response, error) in
+            
+            guard error == nil else {
+                return
+            }
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+
+            self.parseData(data, completionHandlerForParseData: completionHandlerForPOST)
+        }
         
+        task.resume()
+        return task
     }
     
     
     
+    private func parseData(data: NSData, completionHandlerForParseData: (result: AnyObject?, error: String?)-> Void) {
+        
+        let result: AnyObject?
+        
+        do {
+            result = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+        } catch {
+            completionHandlerForParseData(result: nil, error: "Failed to Parse Data")
+            return
+        }
+        
+        completionHandlerForParseData(result: result, error: nil)
+    }
     
-    
-    
-    
-    private func buildURL(host: String, method: String, parameters: [String: AnyObject]) -> NSURL? {
+    private func urlFromComponents(scheme: String, host: String, method: String, parameters: [String: AnyObject]) -> NSURL {
         let url = NSURLComponents()
-        url.scheme = "https"
+        url.scheme = scheme
         url.host = host
         url.path = method
-        //url.queryItems =
+        url.queryItems = [NSURLQueryItem]()
         
-        return url.URL
+        for (key, value) in parameters {
+            let query = NSURLQueryItem(name: key, value: "\(value)")
+            url.queryItems?.append(query)
+        }
+        
+        return url.URL!
     }
     
     class func sharedInstance() -> OTMClient {
